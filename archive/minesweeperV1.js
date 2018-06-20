2e03e7da77fad3826bb9dc91d495b32fab733cbf
Devlin Verley <verle006@umn.edu>
2018-06-19 19:59:02 -0500

    Initial commit

---
+++ archive/minesweeperV1.js
@@ -0,0 +1,8 @@
+const blankLine = '  |   |  ';
+
+console.log(`This is what an empty board would look like:\n ${blankLine}\n ${blankLine}\n ${blankLine}`);
+
+const guessLine = '1 |   |  ';
+const bombLine = '  | B |  ';
+
+console.log(`This is what a board with a guess and a bomb would look like:\n ${guessLine}\n ${bombLine}\n ${blankLine}`);
---
+++ archive/minesweeperV3.js
@@ -0,0 +1,56 @@
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    let board = [];
+
+    for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+        let row = [];
+
+        for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+            row.push(' ');
+        }
+
+        board.push(row);
+    }
+
+    return board;
+};
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    let board = [];
+
+    for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+        let row = [];
+
+        for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+            row.push(null);
+        }
+
+        board.push(row);
+    }
+
+    let numberOfBombsPlaced = 0;
+
+    while (numberOfBombsPlaced < numberOfBombs) {
+        // Duplicate bombs possible prior to control flow
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+
+        let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+
+        board[randomRowIndex][randomColumnIndex] = 'B';
+
+        numberOfBombsPlaced++;
+    }
+
+    return board;
+};
+
+const printBoard = board => {
+    console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+let playerBoard = generatePlayerBoard(3,4);
+
+let bombBoard = generateBombBoard(3,4,5);
+
+console.log(`Player Board: ${printBoard(playerBoard)}`);
+
+console.log(`Bomb Board: ${printBoard(bombBoard)}`);
